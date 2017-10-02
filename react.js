6a9e706fbc3cae227c913478ed7588f9639b07ef
John Grishin <r@exah.ru>
2017-10-02 14:19:00 +0300

    :sparkles: Extend eslint-plugin-react recommended

--- react.js
+++ react.js
@@ -1,11 +1,14 @@
 const config = require('./index')
 
 module.exports = {
-  extends: config.extends.concat([ 'standard-jsx' ]),
+  extends: config.extends.concat([ 'plugin:react/recommended', 'standard-jsx' ]),
   globals: config.globals,
   rules: Object.assign({}, config.rules, {
-    'react/react-in-jsx-scope': 2,
+    'react/prop-types': 0,
+    'react/no-find-dom-node': 0,
+    'react/no-deprecated': 1,
     'react/jsx-handler-names': 1,
+    'react/jsx-key': 1,
     'react/jsx-sort-props': [
       1,
       {
@@ -15,11 +18,10 @@ module.exports = {
         reservedFirst: [ 'key', 'ref' ]
       }
     ],
-    'react/jsx-key': 1,
+    'react/react-in-jsx-scope': 2,
     'react/jsx-closing-bracket-location': 2,
     'react/jsx-no-comment-textnodes': 2,
     'react/jsx-closing-tag-location': 2,
-    'react/no-deprecated': 1,
     'react/jsx-no-bind': [
       2,
       {
